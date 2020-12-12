1ce82b0a7e5904e4c506874b8478843565e77959
SergeySokolov-20 <72944422+SergeySokolov-20@users.noreply.github.com>
2020-12-12 07:10:22 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+   $('.slider').slick({
+      arrows: true,
+      dots: true,
+      slidesToShow: 1,
+      centerMode: true,
+      centerPadding: '0px',
+      autoplay: true,
+      speed: 1000,
+      autoplaySpeed: 1200,
+      infinite: true,
+      adaptiveHeight: true,
+   });
+});
